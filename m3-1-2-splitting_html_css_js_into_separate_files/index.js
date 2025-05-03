d9bf4525f8fe030c424beec30da2a95d31619d86
tarak6984 <tareksabbir20@gmail.com>
2025-05-03 16:53:09 +0600

    solve practice problems

--- m3-1-2-splitting_html_css_js_into_separate_files/index.js
+++ m3-1-2-splitting_html_css_js_into_separate_files/index.js
@@ -1,1 +1,8 @@
 // Bring your JavaScript here
+
+const logoDiv = document.querySelector('#logo')
+const logoImg = document.createElement('img')
+logoDiv.appendChild(logoImg)
+logoDiv.style.maxWidth = '100px'
+logoImg.src = 'logo.png'
+logoImg.style.width = '100%'
